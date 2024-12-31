dfca3a3a9edf49479b9220e7fe846e2e115a004f
ramaputra1 <166339284+ramaputra1@users.noreply.github.com>
2024-12-30 22:56:59 -1000

    For Loop & While Loop

--- 26-ForLoop/script.js
+++ 26-ForLoop/script.js
@@ -9,3 +9,8 @@ for (; counter <= 10; ) {
   document.writeln(`<p>Hello World ${counter}</p>`);
   counter++; // we need that to have false condition so it will be stop
 }
+
+// Full
+for (let counter = 1; counter <= 10; counter++) {
+  document.writeln(`<p>Hello ${counter}</p>`);
+}
---
+++ 27-while-loop/script.js
@@ -0,0 +1,6 @@
+let counter = 1; // Init
+while (counter <= 10) {
+  // Only condition here
+  document.writeln(`<p>Hello Worl ${counter}</p>`);
+  counter++; // Post
+}
