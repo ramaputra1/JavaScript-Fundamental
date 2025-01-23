f98a3bd6df341eb42ea1df9edcb44a003a92f928
ramaputra1 <rama.aptra@gmail.com>
2025-01-23 05:35:02 -1000

    Error Handling

---
+++ OOP/23-Error-Handling/script.js
@@ -0,0 +1,76 @@
+{
+  // 1st
+  class MathUtil {
+    static sum(...numbers) {
+      if (numbers.length === 0) {
+        throw new Error("Total parameter harus lebih dari 0");
+      }
+
+      let total = 0;
+      for (const number of numbers) {
+        total += number;
+      }
+      return total;
+    }
+  }
+
+  try {
+    console.info(MathUtil.sum()); // the sum is error
+    console.info("Eko");
+  } catch (error) {
+    // then catch will execute cause we found error
+    console.info(`Terjadi error : ${error.message}`);
+  }
+
+  console.info("Khannedy");
+}
+
+{
+  // 2nd
+  class MathUtil {
+    static sum(...numbers) {
+      if (numbers.length === 0) {
+        throw new Error("Total parameter harus lebih dari 0");
+      }
+
+      let total = 0;
+      for (const number of numbers) {
+        total += number;
+      }
+      return total;
+    }
+  }
+
+  try {
+    console.info(MathUtil.sum());
+    console.info("Eko");
+  } catch (error) {
+    console.info(`Terjadi error : ${error.message}`);
+  } finally {
+    console.info("Program selesai"); // this one will always be execute after even for error or no error
+  }
+
+  console.info("Khannedy");
+}
+
+{
+  // try finally // just to be more effective cause we do not use catch anymore instead of straight go with the finally
+  class Counter {
+    #counter = 1;
+
+    next() {
+      try {
+        return this.#counter;
+      } finally {
+        this.#counter++;
+      }
+    }
+  }
+
+  const counter = new Counter();
+  console.info(counter.next());
+  console.info(counter.next());
+  console.info(counter.next());
+  console.info(counter.next());
+  console.info(counter.next());
+}
